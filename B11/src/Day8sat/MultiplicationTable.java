a6663557c91c68d3829d572c4c5f445899398de4
spoorthi gowda <spoorthigowda504@gmail.com>
2022-03-24 17:05:20 +0530

    8th commit

---
+++ B11/src/Day8sat/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package Day8sat;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) 
+	{
+		int num,i;
+		System.out.println("Multiplication Table");
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		for(i=1;i<=10;i++)
+		{
+			System.out.println(num+"*"+i+"="+(num*i));
+		}
+
+	}
+
+}
---
+++ B11/src/Day8sat/PrimeNumber.java
@@ -0,0 +1,29 @@
+package Day8sat;
+
+public class PrimeNumber {
+
+	public static void main(String[] args)
+	{
+		int n=10;
+		for(int i=2;i<=n;i++)
+		{
+			int count=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==0)
+			{
+				System.out.println(i+" ");
+				
+			}
+				
+		}
+
+	}
+
+}
+
