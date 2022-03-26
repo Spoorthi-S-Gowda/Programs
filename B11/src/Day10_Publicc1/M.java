c800616a93abe2fe64f30d8165295bb94fa8be7a
spoorthi gowda <spoorthigowda504@gmail.com>
2022-03-26 08:29:08 +0530

    Public Modifier

---
+++ B11/src/Day10_Publicc1/M.java
@@ -0,0 +1,12 @@
+
+// Public Modifier
+package Day10_Publicc1;
+
+public class M 
+{
+	public void display() 
+    { 
+        System.out.println(" Public Modifier "); 
+    } 
+
+}
---
+++ B11/src/Day10_Publicc2/N.java
@@ -0,0 +1,15 @@
+
+// Public Modifier
+package Day10_Publicc2;
+import Day10_Publicc1.*;
+
+//  accessible from everywhere in the program
+class N 
+{
+	public static void main(String args[])
+    {
+        M obj = new M();
+        obj.display();
+    }
+
+}
