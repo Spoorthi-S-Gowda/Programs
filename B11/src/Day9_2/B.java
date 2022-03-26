b5308c7170c488bd7c446b0eee5d8a38ab250b55
spoorthi gowda <spoorthigowda504@gmail.com>
2022-03-26 07:05:09 +0530

    9th-2 commit

---
+++ B11/src/Day9_2/B.java
@@ -0,0 +1,13 @@
+package Day9_2;
+//package that import the packagename.*
+import Day9_1.*;
+
+public class B 
+{
+	public static void main(String args[])
+	{  
+		   A obj = new A();  
+		   obj.msg();  
+	}  
+
+}
